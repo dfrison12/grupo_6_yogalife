2001b2251778df9bcee2f20efc1bf9c9177b0c3e
RodrigoAller <ro1989@gmail.com>
2021-11-06 19:06:32 -0300

    Mi primer avance del trabajo de desarrollo web

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+
 app.listen(3000, () => console.log('Servidor funcionando en puerto 3000'));
