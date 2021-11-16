186e547bb9dc711e4841f7b3e886b9bdfb0b682a
MarianoSanguinetti <marianosanguinetti74@yahoo.com.ar>
2021-11-16 20:35:08 -0300

    le vamos metiendo

--- app.js
+++ app.js
@@ -12,5 +12,8 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
+app.get('/shoppingcart', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/shopcart.html"))
+});
 
 app.listen(3000, () => console.log('Servidor funcionando en puerto 3000'));
