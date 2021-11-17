d63ba1cf80c0695b7fc819c29effc8661adc6d97
Dario <dariohfrison@gmail.com>
2021-11-17 00:31:51 -0300

    add product-description

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ app.get('/shoppingcart', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/shopcart.html"))
 });
 
+app.get('/product-description', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/product-description.html"))
+});
+
 app.listen(3000, () => console.log('Servidor funcionando en puerto 3000'));
