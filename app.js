e3c5c55b1ca43d70d2e9235d8c8b0b618d9c6abe
MarianoSanguinetti <marianosanguinetti74@yahoo.com.ar>
2021-11-25 20:08:20 -0300

    shopcart e index

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
-app.get('/shoppingcart', (req, res) => {
+app.get('/shopcart', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/shopcart.html"))
 });
 
