45209414d296a7be98cce0cdcd0c3d39df3de756
Dario <dariohfrison@gmail.com>
2021-12-08 18:22:40 -0300

    update

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -27,18 +27,24 @@ let upload =  multer({storage: multerDiskStorage});
 //Mostrar Catalogo Completo//
 router.get('/', productsController.index);
 
+
 //Buscar prendas en el catalogo//
 router.get('/search', productsController.search);
 
-//Registrar nueva prenda para el catalogo - Formulario
-router.get('/create', productsController.create)
-//Registrar nueva prenda para el catalogo - Guardar
-router.post('/store', upload.any(''), productsController.store)
 
+// - CREAR PRODUCTO
+//--- Registrar nueva prenda para el catalogo - Formulario
+router.get('/create', productsController.create)
+//--- Registrar nueva prenda para el catalogo - Guardar
+router.post('/store', upload.any(''), productsController.store)
 
 
 //Mostrar un producto en particular//
 router.get('/product-detail/:id', productsController.productDetail);
+
+// EDITAR PRODUCTO
+//Editar -- Mostrar formulario 
+//router.get('/edit/:id', productsController.edit);
 
 
 
