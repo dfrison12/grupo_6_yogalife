d6ec49b4e1a9a7c789f1ef331600e3f77f91c2a5
Dario <dariohfrison@gmail.com>
2022-01-08 22:12:06 -0300

    cambios

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,9 @@
 // -- DATOS PARA TRABAJAR
 
+const { ifError } = require('assert');
 const fs = require('fs');
 const path = require('path');
+const { CLIENT_RENEG_WINDOW } = require('tls');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 let catalogo = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -92,19 +94,33 @@ const productsController = {
     update: (req,res) => {
         let itemId = req.params.id;
         let productToEdit = catalogo.find( prenda => prenda.id == itemId)
-        let image1 = req.files[0].filename;
-        let image2 = req.files[1].filename;
-        let image3 = req.files[2].filename;
-        let image4 = req.files[3].filename;
+       
+        req.files.forEach( file => {
+            switch(file.fieldname){
+                case "img1":
+                    productToEdit.img1 = file.originalname;
+                    break 
+                case "img2":
+                    productToEdit.img2 = file.originalname;
+                    break
+                case "img3":
+                    productToEdit.img3 = file.originalname;
+                    break
+                case "img4":
+                    productToEdit.img4 = file.originalname;
+                    break    
+            }
+
+        } )
+        /*let image1 = req.files[0] ? req.files[0].filename : productToEdit.img1;
+        let image2 = req.files[1] ? req.files[1].filename : productToEdit.img2;
+        let image3 = req.files[2] ? req.files[2].filename : productToEdit.img3;
+        let image4 = req.files[3] ? req.files[3].filename : productToEdit.img4;*/
 
         //Redefinimos la variable con los datos del  Formulario
         productToEdit = {
             id: productToEdit.id,
             ...req.body,
-            img1:image1,
-            img2:image2,
-            img3:image3,
-            img4:image4,
             talle: [1,2,3],
             color: ["bosque-encantado",
             "hora-magica"],
